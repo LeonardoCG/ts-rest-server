87670db91bfec525659b26cb1e45666c2195ab28
LeonardoCG <andresxach7@gmail.com>
2023-03-02 23:48:18 -0600

    rest server TS - routes/usaurios

--- models/server.ts
+++ models/server.ts
@@ -1,13 +1,24 @@
 import express, { Application } from 'express';
+import userRoutes from '../routers/usuario';
 
 class Server {
     // definir el tipo
     private app: Application;
     private port: string;
+    private apiPaths = {
+        usuarios: '/api/usuarios'
+    }
 
     constructor() {
         this.app = express();
         this.port = process.env.PORT || '8000';
+
+        //definir rutas
+        this.routes();
+    }
+
+    routes() {
+        this.app.use(this.apiPaths.usuarios, userRoutes )
     }
 
     listen() {
